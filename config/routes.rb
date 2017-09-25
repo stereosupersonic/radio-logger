7bbd5d81c28bb8fafe738555115d4d6f5de2b00d
Michael Deimel <michael@deimel.de>
2017-09-25 16:59:11 +0200

    add new station

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get "welcome/index"
   root "welcome#index"
+
+  namespace :admin do
+    resources :stations, only: %i[index new create]
+  end
 end
--- spec/features/admin/manage_stations_spec.rb
+++ spec/features/admin/manage_stations_spec.rb
@@ -3,13 +3,13 @@ require "capybara_helper"
 feature "manage stations" do
   scenario "add a new stations" do
     visit "admin/stations"
-
-    click_button "new"
-    #
-    #expect(page).to have_content "Welcome"
+    within "#main" do
+      click_on "New"
+    end
+    # expect(page).to have_content "Welcome"
   end
-  #scenario "list stations" do
-  #scenario "update stations" do
-  #scenario "delete a stations" do
-  #scenario "disable a stations" do
+  # scenario "list stations" do
+  # scenario "update stations" do
+  # scenario "delete a stations" do
+  # scenario "disable a stations" do
 end
