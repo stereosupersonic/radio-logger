6c8601f225a24744922312dbef37b3b80af2e973
Michael Deimel <michael@deimel.de>
2017-09-25 14:35:21 +0200

    rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require "active_support/testing/time_helpers"
 require "webmock/rspec"
 require "vcr"
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -22,45 +22,23 @@ VCR.configure do |c|
 end
 
 RSpec.configure do |config|
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
     end
-
-    config.include ActiveSupport::Testing::TimeHelpers
-    config.include FactoryGirl::Syntax::Methods
-    config.include ControllerHelpers, type: :controller
-    config.include RequestHelpers, type: :request
-    config.include MailerHelpers
-    config.include PermissionHelpers
   end
 end
