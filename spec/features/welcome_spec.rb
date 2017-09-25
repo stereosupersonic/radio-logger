a116a5e9db3f70dd5954d73c1f8500cbf4cc211f
Michael Deimel <michael@deimel.de>
2017-09-25 15:01:55 +0200

    fix rubocop warning

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,7 +1,7 @@
 require "capybara_helper"
 
-feature "welcome" do
-  scenario "with week and trainings" do
+feature "welcome page" do
+  scenario "show the welcome page" do
     visit root_path
 
     expect(page).to have_content "Welcome"
